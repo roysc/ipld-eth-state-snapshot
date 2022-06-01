21bb21bee5a75dc3e6303b83e92717f6e6910fc4
Roy Crihfield <roy@manteia.ltd>
2022-06-01 23:57:09 +0800

    handle sigkill in worker tracker

--- pkg/snapshot/tracker.go
+++ pkg/snapshot/tracker.go
@@ -53,7 +53,7 @@ func newTracker(file string, buf int) iteratorTracker {
 func (tr *iteratorTracker) captureSignal() {
 	sigChan := make(chan os.Signal, 1)
 
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
 	go func() {
 		sig := <-sigChan
 		log.Errorf("Signal received (%v), stopping", sig)
